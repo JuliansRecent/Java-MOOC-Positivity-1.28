c877efcf10b0511fb152d2616e10f89a6b898233
JuliansRecent <juliansrecent@gmail.com>
2023-01-25 22:40:19 -0800

    Module complete. Create a program that prompts the user for a number. If the number is greater than 0, output 'The number is positive.', if the number is negative, output 'The number is not positive.'

--- src/main/java/Positivity.java
+++ src/main/java/Positivity.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 public class Positivity {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        // Prompt the user for a number
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        // If the number is positive print "The number is positive"
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            // If the number is not positive print "The number is not positive."
+            System.out.println("The number is not positive.");
+        }
+
     }
 }
